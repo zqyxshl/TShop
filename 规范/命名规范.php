a35342349f2a229bee140531f197fb5ac57b7378
zqyxshl <zqyxshl@qq.com>
2016-04-13 14:00:11 +0800

    第一次上传文件
    第一次上传文件

---
+++ 规范/命名规范.php
@@ -0,0 +1,12 @@
+<?php
+	变量命名
+		小写开头,驼峰命名		例:	variableName
+	
+	方法命名
+		大写开头,驼峰命名		例:	FunctionName
+			按照功能命名
+				获得XXX从XXX		GetSomethingFromSomewhere		例:	GetFieldFromTable
+				获得XXX以XXX		GetSomethingBySomething			例:	GetFieldFromField
+				做XXX为XXX		DosomethingForSomething			例:	ChangeStatForDelete
+				
+?>
